ea3e754d64a050104e888dbe3eee910b1688891e
Akhamis1 <ammar779@outlook.com>
2026-09-17 17:36:45 +0300

    feat: add Client model

---
+++ models/Client.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+
+const clientSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:String,
+        required:true
+    },
+    company:{
+        type:String
+    },
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }
+},{timestamps:true})
+
+const Client = mongoose.model("Client",clientSchema)
+
+module.exports = Client
