098c73d70b7cf1fb54228fd45da8c56da95f7331
Akhamis1 <ammar779@outlook.com>
2026-09-17 17:48:22 +0300

    feat: add Project model

---
+++ models/Project.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose")
+
+const projectSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    status:{
+        type:String,
+        enum:["Pending","In Progress","Completed","Cancel"],
+        default:"Pending"
+    },
+    deadline:{
+        type:Date
+    },
+    client:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Client"
+    },
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }
+},{timestamps:true})
+
+const Project = mongoose.model("Project",projectSchema)
+
+module.exports = Project
