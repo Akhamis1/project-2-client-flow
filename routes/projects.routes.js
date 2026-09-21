62a5e81054c85bd47058e9ffe9b4cba89ab1a36a
Akhamis1 <ammar779@outlook.com>
2026-09-21 16:19:47 +0300

    feat: add create project feature and save it in the database

--- routes/projects.routes.js
+++ routes/projects.routes.js
@@ -4,19 +4,41 @@ const Project = require("../models/Project")
 const Client = require("../models/Client")
 const isSignedIn = require("../middleware/is-signed-in")
 
-// to create a new project
+// shows the form to create a new project
 router.get("/new", isSignedIn, async (req,res)=>{
     try{
-        const clients = await Client.find({
-            owner: req.session.user._id
-        })
+    const clients = await Client.find({
+    owner: req.session.user._id
+    })
 
-        res.render("projects/create-project.ejs", {clients})
+    res.render("projects/create-project.ejs", {clients})
 
     }catch (error){
-        console.log(error)
-        res.redirect("/")
+    console.log(error)
+    res.redirect("/")
     }
 })
+// create the project to save it in the database
+router.post("/",isSignedIn,async (req,res)=>{
+    try{
+    await Project.create({
+    title: req.body.title,
+    description: req.body.description,
+    price: req.body.price,
+    status: req.body.status,
+    deadline: req.body.deadline,
+    client: req.body.client,
+    owner: req.session.user._id
+    
+    })
+    res.redirect("/projects")
+    
+    } catch (error) {
+    console.log(error)
+    res.redirect("/projects/new")
+    }
+})
+
+
 
 module.exports = router
